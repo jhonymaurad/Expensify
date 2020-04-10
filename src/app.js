977567e5780c2f18ea7cc81f52f3c8d68d56d77f
jhony maurad <jhonymaurad@gmail.com>
2020-04-09 23:15:06 -0400

    removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
@@ -13,34 +12,6 @@ import './styles/styles.scss';
 import 'react-dates/initialize';
 
 const store = configureStore();
-
-const expenseOne = store.dispatch(
-  addExpense({
-    description: 'Water bill',
-    amount: 100,
-    createdAt: 1000
-  })
-);
-
-const expenseTwo = store.dispatch(
-  addExpense({
-    description: 'Gas bill',
-    amount: 50,
-    createdAt: 5000
-  })
-);
-
-const expenseThree = store.dispatch(
-  addExpense({
-    description: 'Rent',
-    amount: 109500,
-    createdAt: 2000
-  })
-);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 ReactDOM.render(
   <Provider store={store}>
